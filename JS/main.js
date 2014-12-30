d2b138e5346711e56342534a2709ee2b1fa08835
gaofanfan <1125774809@qq.com>
2014-12-30 11:03:12 +0800

    add RADME.md file

--- JS/main.js
+++ JS/main.js
@@ -6,6 +6,7 @@ var Weapon = require('./model/weapon');
 function WeaponEvolution() {
   var armor = new Armor('护盾', 5);
   var weapon = new Weapon('尚方宝剑', 4);
+
   var player1 = new Player('张三' , 100 , 10);
   var player2 = new Player('李四' , 80 , 8);
   var playerpk = new PlayerPk(player1 , player2);
